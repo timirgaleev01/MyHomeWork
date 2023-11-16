664e6eb280f17fcf56b6da7fa89f0224db8b88d6
Denis Timirgaleev <timirgaleev.dan@gmail.com>
2023-11-16 22:24:51 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Vladimir";
+        String surname = "Ivanov";
+        int age = 23;
+        String name2 = "Denis";
+        String surname2 = "Timirgaleev";
+        int age2 = 24;
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("My name " + name + " " + surname + "and I'm " + age + "years old");
+
+        }
+        for (int i = 0; i < 6; i++) {
+            System.out.println("My name " + name2 + " " + surname2 + "and I'm " + age2 + "years oldd");
+        }
+
+    }
+}
